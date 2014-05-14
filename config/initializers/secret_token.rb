6f6edd4ae5cf7a68b81690d9f0f8b33f3ac47f6c
vijayajyothi <jyothis.kalangi@gmail.com>
2014-05-14 03:09:41 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vmdn::Application.config.secret_token = '516ec7a0b0ed0d28703a5c0de99f8d494355611369f6729bf852ca6b128b540e63bfe00e291238416a120388afe47fa9be78ed4f9743b5d641fc27a86c789368'
